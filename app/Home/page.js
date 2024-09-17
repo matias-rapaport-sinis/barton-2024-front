ace7d12000bd601a6144b40ec2268cee9d7c8229
Tomás Lostes <tomylostes@gmail.com>
2024-09-17 14:23:57 -0300

    El componente Image con la propiedad fill deberia cubrir al padra, pero por algun motivo cubre toda la pagina. El error eesta en Home/Page en el Image de la linea 15

--- app/Home/page.js
+++ app/Home/page.js
@@ -1,4 +1,59 @@
-export default function HomePage(){
+import Image from "next/image";
 
-    return <p className="fs-1">Esto es el Home</p>;
+export default function HomePage() {
+
+    return (
+        <main className="container-fluid p-0 h-100">
+          <div className="row-12">
+                    <iframe className="h-100 w-100" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+          </div>
+          <div className="row">
+            <p className="fs-1 text-center font-barton p-3 text-color-barton-brand"> BARTON EN TU PROXIMA OBRA</p>
+          </div>
+            <div className=" d-flex flex-coloumn " style={{minHeight : "50vh"}}>
+              <div className="flex-grow-1 mx-1" style={{height : 300, widht : 200}} > 
+              <Image
+                    src={"/constructor.png"}
+                    alt="Logo de Barton"
+                    fill
+                    objetjFit="contain"
+                  /> 
+              </div>
+              <div className="flex-grow-1 mx-1">
+              <Image
+                    src={"/constructor.png"}
+                    alt="Logo de Barton"
+                    width={200}
+                    height={200}
+                  />
+              </div>       
+              <div className="flex-grow-1 mx-1">
+                <Image
+                    src={"/constructor.png"}
+                    alt="Logo de Barton"
+                    width={200}
+                    height={200}
+                  />
+                </div>       
+              <div className="flex-grow-1 mx-1">
+                <Image
+                    src={"/constructor.png"}
+                    alt="Logo de Barton"
+                    width={200}
+                    height={200}
+                  />
+                </div>       
+              <div className="flex-grow-1 mx-1">
+                <Image
+                    src={"/constructor.png"}
+                    alt="Logo de Barton"
+                    width={200}
+                    height={200}
+                  />
+                </div>       
+
+            </div>
+
+        </main>
+    );
 }
--- app/_components/Navbar/Navbar.js
+++ app/_components/Navbar/Navbar.js
@@ -1,43 +1,91 @@
 import Image from 'next/image';
 
+
+
+
+function NavbarItem({ content, href }) {
+    return (
+        <li className="nav-item px-3">
+            <a className="nav-link  text-color-barton active font-barton" aria-current="page" href={href}>
+                {content}
+            </a>
+        </li>
+
+    );
+}
+
+function DropdownItem({ content, href }) {
+    return (
+        <li><a className="dropdown-item" href={href}>{content}</a></li>
+    )
+}
+
+function NavbarItemDropdown({ content, children }) {
+    return (
+        <li className="nav-item px-3 dropdown">
+            <a className="nav-link dropdown-toggle active text-color-barton font-barton" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {content}
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                {children}
+            </ul>
+        </li>
+
+    );
+}
+
+function NavbarItemIcon({icon, alt}) {
+    return (
+        <li className="nav-item px-3">
+            <a className="nav-link">
+                <Image
+                    src={icon}
+                    alt={alt}
+                    width={38}
+                    height={34}
+                />
+            </a>
+        </li>
+
+    );
+}
 export default function Navbar() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-color-barton ">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
-                    <Image 
+                    <Image
                         src={"/logo.png"}
                         alt="Logo de Barton"
-                        width={100}
-                        height={60}
-                     />
+                        width={206}
+                        height={86}
+                    />
                 </a>
+               
                 <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <div className="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto">
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle text-color-barton" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Productos
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-color-barton" href="#">Aplicaciones</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-color-barton active" aria-current="page" href="#">Documentacion</a>
-                        </li>                        
-                        <li className="nav-item">
-                            <a className="nav-link text-color-barton" href="#" tabIndex="-1" aria-disabled="true">Contacto</a>
-                        </li>
+
+                        <NavbarItemDropdown content="Producto">
+                            <DropdownItem content="item1" href="#" />
+                            <DropdownItem content="item2" href="#" />
+                            <DropdownItem content="item3" href="#" />
+                        </NavbarItemDropdown>
+
+                        <NavbarItemDropdown content="Aplicaciones">
+                            <DropdownItem content="item1" href="#" />
+                            <DropdownItem content="item2" href="#" />
+                            <DropdownItem content="item3" href="#" />
+                        </NavbarItemDropdown>
+
+                        <NavbarItem content="Documentacion" href="#" />
+                        <NavbarItem content="Contacto" href="#" />
+
+                        <NavbarItemIcon icon="/icono-changuito.png" alt="Icono de chango" />
                     </ul>
                 </div>
             </div>
