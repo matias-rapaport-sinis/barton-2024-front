a85baaa067f051b28535c987ce5c3173e79ecdc1
Tomás Lostes <tomylostes@gmail.com>
2024-09-17 10:21:19 -0300

    Puede encontrar la propiedad y elemento a modificar para desplazar la lista del Navbar a la izquierda.

--- app/_components/Navbar/Navbar.js
+++ app/_components/Navbar/Navbar.js
@@ -16,9 +16,9 @@ export default function Navbar() {
                 <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item dropdown">
+                <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle text-color-barton" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Productos
                             </a>
