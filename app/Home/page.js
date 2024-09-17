aed902335ad7590c8dd66b1d788391a5bf209ac5
rapa-matias-sinis <matias.rapaport@sinis.com.ar>
2024-09-17 15:59:34 -0300

    Resolvi porque el video se hizo chico

--- app/Home/page.js
+++ app/Home/page.js
@@ -4,10 +4,10 @@ export default function HomePage() {
 
     return (
         <main className="container-fluid p-0 h-100">
-          <div className="row-12">
-                    <iframe className="h-100 w-100" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+          <div className="row-12 h-100">
+                    <iframe  fill className="h-100 w-100" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
           </div>
-          <div className="row">
+          <div className="row" >
             <p className="fs-1 text-center font-barton p-3 text-color-barton-brand"> BARTON EN TU PROXIMA OBRA</p>
           </div>
             <div className=" d-flex flex-coloumn " style={{minHeight : "50vh"}}>
@@ -53,7 +53,6 @@ export default function HomePage() {
                 </div>       
 
             </div>
-
         </main>
     );
 }
